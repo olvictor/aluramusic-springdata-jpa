d6d0e599ca909d192af9081054f72415b9ed4eca
victorolv <victorolvdev@gmail.com>
2024-05-23 16:28:40 -0300

    criacao da estrutura de pastas

---
+++ src/main/java/com/example/alura_music/AluraMusicApplication.java
@@ -0,0 +1,13 @@
+package com.example.alura_music;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AluraMusicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AluraMusicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/alura_music/AluraMusicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.alura_music;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AluraMusicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
